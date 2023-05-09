3309f8fc8228b0b0346c4698962f31a8e92ae2ca
ZeynabSherafat <Nature4explorer@gmail.com>
2023-05-09 19:45:55 +0430

    Finished the basic HTML and CSS

--- src/Weather.js
+++ src/Weather.js
@@ -19,52 +19,44 @@ export default function Weather(){
             </form>
             <h5><span>Saturday</span>,{" "}<span>5:30</span></h5>
             <h5>May 2023</h5>
+            <h3>Mostly Cloudy</h3>
             <h1>9°C</h1>
             <h2>Karaj, Iran</h2>
             </div>
-            <div className="col-5 " id="forecast">
-          <row>
-            <div className="col border border-primary">
-              Hello
-            </div>
-            <div className="col border border-primary">
-              hi
+            <div className="col-5" id="forecast">
+            <row>
+            <div className="col">
+                <span className="forecast-temp">17°/25°</span>
+                <span className="forecast-date">Sun</span>
+          <hr />
             </div>
           </row>
-          <hr />
           <row>
-            <div className="col border border-primary">
-              Hello
-            </div>
-            <div className="col border border-primary">
-              hi
+            <div className="col">
+                <span className="forecast-temp">12°/16°</span>
+                <span className="forecast-date">Mon</span>
+          <hr />
             </div>
           </row>
-          <hr />
           <row>
-            <div className="col border border-primary">
-              Hello
-            </div>
-            <div className="col border border-primary">
-              hi
+            <div className="col">
+                <span className="forecast-temp">9°/17°</span>
+                <span className="forecast-date">Tue</span>
+          <hr />
             </div>
           </row>
-          <hr />
           <row>
-            <div className="col border border-primary">
-              Hello
-            </div>
-            <div className="col border border-primary">
-              hi
+            <div className="col">
+                <span className="forecast-temp">11°/21°</span>
+                <span className="forecast-date">Wed</span>
+          <hr />
             </div>
           </row>
-          <hr />
           <row>
-            <div className="col border border-primary">
-              Hello
-            </div>
-            <div className="col border border-primary">
-              hi
+            <div className="col">
+                <span className="forecast-temp">13°/18°</span>
+                <span className="forecast-date">Thu</span>
+          <hr />
             </div>
           </row>
           </div>
