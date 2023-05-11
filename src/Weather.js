88d09401a0df63478f9c1d851199a3002633707f
ZeynabSherafat <Nature4explorer@gmail.com>
2023-05-11 19:21:35 +0430

    Set the API for current weather

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,6 @@
 import React, {useState} from "react";
+import axios from 'axios';
+
 import "./Weather.css"
 
 export default function Weather(){
@@ -10,7 +12,6 @@ export default function Weather(){
    let month = months[date.getMonth()];
 
    let year = date.getFullYear();
-   console.log(year);
    let hour = date.getHours();
    let minute = date.getMinutes();
    if (hour < 10){
@@ -21,17 +22,34 @@ export default function Weather(){
    }
 
    let [city, setCity] = useState("")
+   let [weather, setWeather] = useState("")
+   let [on, setOn] = useState(false)
+
+   function displayWeatherInfo(response){
+    setWeather ({
+      temperature: response.data.main.temp,
+humidity: response.data.main.humidity,
+wind: response.data.wind.speed,
+city: response.data.name,
+country: response.data.sys.country,
+description: response.data.weather[0].description
+    })
+
+    setOn(true)
+   }
 
    function handleSubmit(event){
     event.preventDefault();
     let apiKey = "97bed167ec49bff56e6c1b63daef9c86"
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    axios.get(url).then(displayWeatherInfo);
    }
 
    function replaceCity(event){
     setCity(event.target.value)
    }
 
+   if (on === true) {
   return (
         <div className="Weather">
             <div className="container"> 
@@ -50,9 +68,11 @@ export default function Weather(){
             </form>
             <h5><span>{day}</span>,{" "}<span>{hour}:{minute}</span></h5>
             <h5>{month} {year}</h5>
-            <h3>Mostly Cloudy</h3>
-            <h1>9°C</h1>
-            <h2>Karaj, Iran</h2>
+            <h3>{weather.description}</h3>
+            <h6>Humidity: {weather.humidity}%</h6>
+            <h6>Windspeed: {Math.round(weather.wind)} km/h</h6>
+            <h1>{Math.round(weather.temperature)}°C</h1>
+            <h2>{weather.city}, {weather.country}</h2>
             </div>
             <div className="col-5" id="forecast">
             <row>
@@ -94,5 +114,9 @@ export default function Weather(){
           </div>
           </div>
         </div>
-    )
+    )} else {
+      let apiKey = "97bed167ec49bff56e6c1b63daef9c86"
+let karajUrl =  `https://api.openweathermap.org/data/2.5/weather?q=karaj&appid=${apiKey}&units=metric`
+axios.get(karajUrl).then(displayWeatherInfo);
+    }
 }
